289c5b4f2120c2bf18e4fada97183cebdde17f73
Alessio Ferrari <alessio.ferrari@marconirovereto.it>
2025-07-02 16:39:41 +0200

    Feat: add fattoriale code

---
+++ Fattoriale/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int fattoriale(int x) {
+    int res = 1;
+    for (int i = 2; i <= x; i++)
+    {
+        res *= i;
+    }
+    return res;    
+}
+
+
+int main() {
+    int n = fattoriale(5);
+    cout << "Fattoriale di 5 e " << n;
+    
+    
+    return 0;
+}
